7759d0203d80d27fd1adf5f998acc279af727c66
Ravuri Venkata Krishna Reddy <132256623+krishnareddy02@users.noreply.github.com>
2026-07-02 19:56:46 +0530

    Fix shared ride matching to check for compatible pools

--- backend/fikarideshare/rides/routing.py
+++ backend/fikarideshare/rides/routing.py
@@ -2,8 +2,8 @@ from django.urls import re_path
 from . import consumers
 
 websocket_urlpatterns = [
-    # re_path(r'ws/tracking/$', consumers.LocationConsumer.as_asgi()),
-    # re_path(r'ws/safety/sos/(?P<sos_id>[^/]+)/$', consumers.SOSTrackingConsumer.as_asgi()),
-    re_path(r'wss://fika-rideshare-api-90ac04a02a1f.herokuapp.com/ws/tracking/$', consumers.LocationConsumer.as_asgi()),
-    re_path(r'wss://fika-rideshare-api-90ac04a02a1f.herokuapp.com/ws/safety/sos/<sos_id>/$', consumers.SOSTrackingConsumer.as_asgi()),
+    re_path(r'ws/tracking/$', consumers.LocationConsumer.as_asgi()),
+    re_path(r'ws/safety/sos/(?P<sos_id>[^/]+)/$', consumers.SOSTrackingConsumer.as_asgi()),
+#     re_path(r'wss://fika-rideshare-api-90ac04a02a1f.herokuapp.com/ws/tracking/$', consumers.LocationConsumer.as_asgi()),
+#     re_path(r'wss://fika-rideshare-api-90ac04a02a1f.herokuapp.com/ws/safety/sos/<sos_id>/$', consumers.SOSTrackingConsumer.as_asgi()),
 ]
